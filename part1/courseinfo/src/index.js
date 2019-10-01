1bae3f61cec2182581eb5d1a81aecca697474f4b
Mhran-m <mhranmrad81@gmail.com>
2019-10-01 15:33:52 +0200

    step1 changes

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,6 +1,33 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+const course ="Half Half Stack application development"
+const Header = (props) => {
+  return (
+    <h1>
+    {props.courseName}
+      
+    </h1>
+  )
+}
+const Content = (props) => {
+  return (
+    <p>
+      {props.part1Name}{props.exercises1Nuber}
+      {props.part2Name}{props.exercises2Nuber}
+      {props.part3Name}{props.exercises3Nuber}
+      
+    </p>
 
+  )
+}
+const Total = (props) => {
+  return (
+    <p>
+      {props.NumberOfExercises}{props.SumNumberofexercises}
+
+    </p>
+  )
+}
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -9,20 +36,18 @@ const App = () => {
   const exercises2 = 7
   const part3 = 'State of a component'
   const exercises3 = 14
+  const sum = 'number of exerciseses'
+  const SumNumberofexercises = 31
 
   return (
     <div>
-      <h1>{course}</h1>
-      <p>
-        {part1} {exercises1}
-      </p>
-      <p>
-        {part2} {exercises2}
-      </p>
-      <p>
-        {part3} {exercises3}
-      </p>
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      < Header courseName={course} />
+
+      < Content part1Name={part1} />< Content exercises1Nuber={exercises1} />
+      < Content part2Name={part2} />< Content exercises2Nuber={exercises2} />
+      < Content part3Name={part3} />< Content exercises3Nuber={exercises3} />
+      < Total NumberOfExercises={sum} />< Total SumNumberofexercises={exercises1 + exercises2 + exercises3} />
+
     </div>
   )
 }
