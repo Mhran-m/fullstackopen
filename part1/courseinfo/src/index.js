ca6eeb8f6eb8ea93e78f5b55dbeff3d5b7f7cad6
Mhran-m <mhranmrad81@gmail.com>
2019-10-02 19:17:44 +0200

    step2 changing

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -1,52 +1,55 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-const course ="Half Half Stack application development"
+
 const Header = (props) => {
+  console.log(props)
   return (
     <h1>
-    {props.courseName}
-      
+
+      {props.courseName}
+
     </h1>
   )
 }
-const part1 = {
-  name: 'Fundamentals of React',
-  exercises: 10
-}
-const part2 = {
-  name: 'Using props to pass data',
-  exercises: 7
-}
-const part3 = {
-  name: 'State of a component',
-  exercises: 14
-}
-const Part = (props) => {
-  console.log('received props.name: ' + props.name);
-  return (
-      <p>
-          {props.name} {props.exercises}
-      </p>
-  );
-
-}
-
-
-const App = () => {
-
-  const exercises1 = 10
-
-  const exercises2 = 7
-  
-  const exercises3 = 14
+const Content = (props) => {
+  console.log('received props.name: ' + props.parts);
   return (
     <div>
-      <Header courseName={course}/>
-      <Part name={part1.name} exercises={part1.exercises}/>
-      <Part name={part2.name} exercises={part2.exercises}/>
-      <Part name={part3.name} exercises={part3.exercises}/>
+      <p> {props.parts[0].name}{props.parts[0].exercises}</p>
 
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
+      <p>{props.parts[1].name}{props.parts[1].exercises}</p>
+
+      <p> {props.parts[2].name}{props.parts[2].exercises}</p>
+    </div>
+  )
+}
+const App = () => {
+  const course = {
+    name: "Half Half Stack application development",
+
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      },
+    ]
+  }
+
+
+
+  return (
+    <div>
+      <Header courseName={course.name} />
+      <Content parts={course.parts} />
+      <p>Number of exercises {course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises}</p>
     </div>
   )
 }
