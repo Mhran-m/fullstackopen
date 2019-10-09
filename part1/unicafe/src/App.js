569748ffba43ac8b0c7f42cfe057ebf8c751d041
Mhran-m <mhranmrad81@gmail.com>
2019-10-09 19:26:05 +0200

    endring

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,55 +9,61 @@ const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [sum, setSum] = useState(0)
   const [allClicks, setAll] = useState([])
 
   const HandleGoodClick = () => {
     setAll(allClicks.concat())
-    setGood(good+1)
-    
+    setGood(good + 1)
+    setSum(sum + 1)
+
+
   }
   const HandleNeutralClick = () => {
     setAll(allClicks.concat())
-setNeutral(neutral+1)
-  
+    setNeutral(neutral + 1)
+    setSum(sum + 1)
+
+
   }
-  
+
   const HandleBadClick = () => {
     setAll(allClicks.concat())
-    setBad(bad+1)
-    
+    setBad(bad + 1)
+    setSum(sum + 1)
+
+
   }
-  
-  
+
+
 
   return (
-    <div> 
+    <div>
       <h1>
         give feedback
       </h1>
-       <div>
-        
+      <div>
+
         <button onClick={HandleGoodClick}>good</button>
- 
+
         <button onClick={HandleNeutralClick}>neutral</button>
- 
-        
+
+
         <button onClick={HandleBadClick}>bad</button>
- 
-       
+
+
       </div>
       <h1>
         statistics
       </h1>
-     <p>good {good} </p> 
-     <p>neutral {neutral}</p>
-     <p>bad {bad}</p>
+      <p>good {good} </p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+      <p> all {sum}</p>
+      <p>average{(good - bad) / sum}</p>
+      <p>postive {(good / sum) * 100}</p>
       <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
-)
 export default App;
